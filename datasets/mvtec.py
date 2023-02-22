32733a89b37898c07eb22cc5267d529a0861bde4
Ali Ansari <47219029+Allliance@users.noreply.github.com>
2023-02-22 11:13:21 +0330

    Perform changes to be not zero

--- datasets/mvtec.py
+++ datasets/mvtec.py
@@ -25,7 +25,7 @@ mvtec_categories = [
 
 class MVTecDataset(torch.utils.data.Dataset):
   
-    def __init__(self, root, category_index, input_size=224, is_train=True):
+    def __init__(self, root, input_size=224, is_train=True):
         self.image_transform = transforms.Compose(
             [
                 transforms.Resize(input_size),
@@ -33,9 +33,14 @@ class MVTecDataset(torch.utils.data.Dataset):
             ]
         )
         if is_train:
-            self.image_files = glob(
-                os.path.join(root, mvtec_categories[category_index], "train", "good", "*.png")
-            )
+            self.image_files = []
+            self.targets = []
+            for i, category in enumerate(mvtec_categories):
+              new_samples = glob(
+                os.path.join(root, category, "train", "good", "*.png")
+              )
+              self.image_files += new_samples
+              self.targets += [i] * len(new_samples)
         else:
             self.image_files = glob(os.path.join(root, mvtec_categories[category_index], "test", "*", "*.png"))
             self.target_transform = transforms.Compose(
@@ -46,13 +51,28 @@ class MVTecDataset(torch.utils.data.Dataset):
             )
         self.is_train = is_train
 
+        # creating targets
+        if is_train:
+          self.targets = [0 for _ in self.image_files]
+        else:
+          self.targets = []
+          for index in range(len(self.image_files)):
+            image_file = self.image_files[index]
+            if os.path.dirname(image_file).endswith("good"):
+                self.targets.append(0)
+            else:
+                self.targets.append(1)
+
     def __getitem__(self, index):
         image_file = self.image_files[index]
         image = Image.open(image_file)
         image = image.convert('RGB')
         image = self.image_transform(image)
+
+        return image, self.targets[index]
+        
         if self.is_train:
-            return image
+            return image, 
         else:
             label = None
             if os.path.dirname(image_file).endswith("good"):
